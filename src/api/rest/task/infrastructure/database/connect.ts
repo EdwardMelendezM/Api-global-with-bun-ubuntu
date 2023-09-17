4eddfe19f5bbc30b8bf27ac4f1740cebfe44f0f0
edwardMM <192666@unsaac.edu.pe>
2023-09-17 18:51:33 -0500

    modified env to connect bd

--- src/api/rest/task/infrastructure/database/connect.ts
+++ src/api/rest/task/infrastructure/database/connect.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 // Define la URL de conexión a la base de datos
 const url_db = process.env.MONGO_URL!
+console.log(url_db)
 // Configura y establece la conexión a la base de datos
 mongoose.connect(url_db).then(() => {
   
@@ -16,5 +17,6 @@ db.on('error', (error) => {
   console.error('Error de conexión a la base de datos:', error);
 });
 
+console.log("EL ERROR LLEGO HASTA AQUI")
 // Exporta la instancia de conexión para su uso en otros archivos
 export default mongoose;
