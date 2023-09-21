305e8623434775057d6adc4b7076a2c2c80ea7f3
edwardMM <192666@unsaac.edu.pe>
2023-09-20 21:48:31 -0500

    adding swagger

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,34 @@ import { Elysia } from "elysia";
 import { MongoTaskRepository } from "./api/rest/task/infrastructure/task.repository.implement";
 import { TaskModel } from "./api/rest/task/domain/model/task.model";
 
+import { swagger } from '@elysiajs/swagger'
 
 const mongoTaskRepository = new MongoTaskRepository()
 
 const app = new Elysia()
-  .group('task', app =>
+  .use(swagger({
+    documentation: {
+      tags: [
+        { name: 'App', description: 'General endpoints' },
+        { name: 'Auth', description: 'Authentication endpoints' }
+      ],
+      info:{
+        title:'Elysia documentation',
+        version:'1.0.0'
+      }
+    }
+  }))
+  .group('task', (app) =>
     app
-    .get("/", () => mongoTaskRepository.getAllTasks())
-    .get("/:id", (ctx) => mongoTaskRepository.getTaskById(ctx.params.id as string))
+    .get("/", () => mongoTaskRepository.getAllTasks(),
+          { detail: {tags:['Task']} })
+    .get("/:id", (ctx) => mongoTaskRepository.getTaskById(ctx.params.id as string),
+          { detail: { tags: ['Task'] } })
     .post("/", (ctx) => mongoTaskRepository.createTask(ctx.body as TaskModel))
-    .patch("/",(ctx) => mongoTaskRepository.updateTask(ctx.body as TaskModel))
+    .patch("/", (ctx) => mongoTaskRepository.updateTask(ctx.body as TaskModel))
     .delete("/:id", (ctx) => mongoTaskRepository.removeTask(ctx.params.id as string))
+    ,
+    
   ).listen(3000);
   
 
